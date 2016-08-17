9688a3c0a5afb159427aaeeae27005a9f18a3217
epsilonrt <epsilonrt@gmail.com>
2016-08-17 18:54:19 +0200

    Module blyss fonctionnel en emission 433MHz

--- sysio/blyss.h
+++ sysio/blyss.h
@@ -1,5 +1,5 @@
 /**
- * @file 
+ * @file
  * @brief Modules Blyss™
  *
  * Copyright © 2016 epsilonRT, All rights reserved.
@@ -11,42 +11,222 @@
 #include <sysio/defs.h>
 __BEGIN_C_DECLS
 /* ========================================================================== */
+#include <stdio.h>
+
 /**
  * @defgroup sysio_blyss Modules Blyss™
  * Ce module permet de gérer la transmission vers des modules domotiques
  * Blyss™ commerciailisés par Castorama (protocole LiveEz™ 433.92 MHz). \n
- * La plateforme doit disposer d'un GPIO.
+ * La plateforme doit disposer d'un GPIO. \n
+ * Ce code est basé sur le travail de « reverse engineering » de 7 membres du
+ * forum arduino.cc: SkyWodd, Artouste, Al1fch, Barbudor, Bunyman, Trigger et
+ * Bidle (https://skyduino.wordpress.com/2012/07/19/hack-partie-2-reverse-engineering-des-interrupteurs-domotique-blyss) \n
+ * @todo Seule la partie emission est implémentée pour l'instant.
  * @{
  */
 /* macros =================================================================== */
-/* constants ================================================================ */
-/* structures =============================================================== */
-/* types ==================================================================== */
-/* private variables ======================================================== */
-/* private functions ======================================================== */
-/* public variables ========================================================= */
-/* internal public functions ================================================ */
-
-// -----------------------------------------------------------------------------
-#  if defined(__DOXYGEN__)
-/* 
- * __DOXYGEN__ defined
- * Partie documentation ne devant pas être compilée.
- * =============================================================================
- */
 /**
- *   @}
- * @}
+ * @brief Affiche le contenu de la trame pointée par f sur stdout
+ * 
+ * @param f pointeur sur une trame de type \ref xBlyssFrame
  */
-#  else
-/* 
- * __DOXYGEN__ not defined
- * Partie ne devant pas être documentée.
- * =============================================================================
- */
-#  endif /* __DOXYGEN__ not defined */
+#define vBlyssPrintFrame(f) vBlyssPrintFrameToFile (f, stdout)
 
+/* constants ================================================================ */
+
+/* structures =============================================================== */
+/**
+ * @class xBlyssFrame
+ * @brief Classe permattant de manipuler une liaison LiveEz
+ * Classe opaque pour l'utilisateur
+ */
+struct xBlyss;
+
+/**
+ * @class xBlyssFrame
+ * @brief Trame LiveEz
+ * 
+ * La trame RF est précédée d'un état haut qui dure 2.4 ms et suivie d'un état
+ * bas de 24 ms, elle contient les informations suivant:
+ * - drapeau de début de trame 0xFE (8 bits),
+ * - canal global (4 bits) de la télécommande,
+ * - adresse (16 bits) de la télécommande,
+ * - sous-canal (4 bits) de l'élément commandé,
+ * - état de l'élément commandé (état logique) (4 bits),
+ * - rolling code, MSBFIRST (8 bits),
+ * - timestamp incrémentiel (0 ~ 255), MSBFIRST (8 bits),
+ * - .
+ * 
+ * Une trame contient donc 13 quartes soit 52 bits stockés dans 7 octets.
+ */
+struct xBlyssFrame {
+  
+  uint8_t raw[7];
+};
+
+
+/* types ==================================================================== */
+/**
+ * @brief xBlyss
+ */
+typedef struct xBlyss xBlyss;
+
+/**
+ * @brief xBlyssFrame
+ */
+typedef struct xBlyssFrame xBlyssFrame;
+
+/* internal public functions ================================================ */
+/**
+ * @brief Création d'une liaison LiveEz
+ * 
+ * @param iOutPin Numéro de la broche reliée à l'émetteur, -1 si pas d'émetteur
+ * @param iInPin Numéro de la broche reliée au récepteur, -1 si pas de récepteur
+ * @return pointeur sur la liaison initialisée, NULL si erreur
+ */
+xBlyss * xBlyssNew (int iOutPin, int iInPin);
+
+/**
+ * @brief Fermeture et destruction d'une liaison LiveEz
+ * 
+ * @param b pointeur sur la liaison
+ * @return 0, -1 si erreur
+ */
+int iBlyssDelete (xBlyss * b);
+
+/**
+ * @brief Envoi d'une trame
+ * 
+ * @param b pointeur sur la liaison
+ * @param frame pointeur sur la trame
+ * @param repeat nombre de répétition d'nevoi
+ * @return 0, -1 si erreur
+ */
+int iBlyssSend (xBlyss * b, xBlyssFrame * frame, uint8_t repeat);
+
+/**
+ * @brief Création et initialisation d'une trame LiveEz
+ * 
+ * La trame devra être libérée avec free()
+ * @param tx_id pointeur sur un tableau de 3 octets contenant dans l'ordre
+ * le canal global, le poids fort de l'adresse et le poids faible de l'adresse
+ * identifiant l'emetteur.
+ * @return pointeur sur la trame initialisée, NULL si ereeur
+ */
+xBlyssFrame * xBlyssNewFrame (const uint8_t * tx_id);
+
+/**
+ * @brief Imprime le contenu d'une trame dans un fichier
+ * 
+ * Le contenu imprimé pour chaque trame est formaté comme ci-dessous:
+ * @code
+ * RF frame          : FE5082410987D
+ * RF footprint      : FE - Ok
+ * RF global channel : 05
+ * RF adress         : 0824
+ * RF channel        : Ch4
+ * Light status      : On
+ * Rolling code      : 98 - Ok
+ * Token             : 7D
+ * @endcode 
+ * @param frame pointeur sur la trame
+ * @param out pointeur sur le fichier ouvert en écriture
+ */
+void vBlyssPrintFrameToFile (const xBlyssFrame * frame, FILE * out);
+
+/**
+ * @brief Modification de l'identifiant de l'émetteur d'une trame
+ * 
+ * @param frame pointeur sur la trame
+ * @param tx_id pointeur sur un tableau de 3 octets contenant dans l'ordre
+ * le canal global (4 bits de poids faible), le poids fort de l'adresse et 
+ * le poids faible de l'adresse identifiant l'emetteur.
+ */
+void vBlyssSetTxId (xBlyssFrame * frame, const uint8_t * tx_id);
+
+/**
+ * @brief Modification du canal destinataire d'une trame
+ * 
+ * @param frame pointeur sur la trame
+ * @param channel canal destinataire codé sur les 4 bits de poids faible 
+ * uniquement, valeur comprise entre 0 et 8, 0 correspondant à tous les canaux
+ * (broadcast), 1 au canal 1, 2 au canal 2 ...
+ */
+void vBlyssSetChannel (xBlyssFrame * frame, uint8_t channel);
+
+/**
+ * @brief Modification de l'état du destinataire
+ * 
+ * @param frame pointeur sur la trame
+ * @param state état du destinataire ON = true
+ */
+void vBlyssSetState (xBlyssFrame * frame, bool state);
+
+/**
+ * @brief Canal destinataire de la trame
+ * 
+ * @param frame pointeur sur la trame
+ * @return canal destinataire codé sur les 4 bits de poids faible 
+ * uniquement, valeur comprise entre 0 et 8, 0 correspondant à tous les canaux
+ * (broadcast), 1 au canal 1, 2 au canal 2 ...
+ */
+uint8_t ucBlyssChannel (const xBlyssFrame * frame);
+
+/**
+ * @brief Etat du destinataire de la trame
+ * 
+ * @param frame pointeur sur la trame
+ * @return état du destinataire ON = true
+ */
+bool bBlyssState (const xBlyssFrame * frame);
+
+/**
+ * @brief Modification du canal global d'une trame
+ * 
+ * @param frame pointeur sur la trame
+ * @param global_channel canal global (4 bits de poids faible)
+ */
+void vBlyssSetGlobalChannel (xBlyssFrame * frame, uint8_t global_channel);
+
+/**
+ * @brief Modification de l'adresse source d'une trame
+ * 
+ * @param frame pointeur sur la trame
+ * @param address adresse de l'émetteur de la trame sur 16 bits
+ */
+void vBlyssSetAddress (xBlyssFrame * frame, uint16_t address);
+
+/**
+ * @brief Canal global d'une trame
+ * 
+ * @param frame pointeur sur la trame
+ * @return canal global (4 bits de poids faible)
+ */
+uint8_t ucBlyssGlobalChannel (const xBlyssFrame * frame);
+
+/**
+ * @brief Adresse source d'une trame
+ * 
+ * @param frame pointeur sur la trame
+ * @return adresse de l'émetteur de la trame sur 16 bits
+ */
+uint16_t usBlyssAddress (const xBlyssFrame * frame);
+
+/**
+ * @brief Test si un buffer contient une trame LiveEz valide
+ * 
+ * @param buffer pointeur sur une zone contenant les octets
+ * @return true si valide
+ */
+bool bBlyssIsValidFrame (const void * buffer);
+
+/**
+ * @brief Test si un canal est valide
+ * 
+ * @param channel canal à tester
+ * @return true si valide
+ */
+bool bBlyssIsValidChannel (uint8_t channel);
 /* ========================================================================== */
 __END_C_DECLS
 #endif /*_SYSIO_BLYSS_H_ defined */
-
--- sysio/src/gpio/blyss.c
+++ sysio/src/gpio/blyss.c
@@ -1,21 +1,484 @@
 /**
- * @file 
- * @brief 
+ * @file
+ * @brief
  *
  * Copyright © 2015 epsilonRT, All rights reserved.
  * This software is governed by the CeCILL license <http://www.cecill.info>
  */
+#include <stdlib.h>
 #include <sysio/blyss.h>
+#include <sysio/doutput.h>
+#include <sysio/dinput.h>
+#include <sysio/delay.h>
 
 /* macros =================================================================== */
+
 /* constants ================================================================ */
+#define BLYSS_FLAG 0xFE
+#define BLYSS_NOF_BITS 52
+#define BLYSS_IDX_FLAG  0
+#define BLYSS_IDX_GCHAN 8
+#define BLYSS_IDX_ADDR  12
+#define BLYSS_IDX_CHAN  28
+#define BLYSS_IDX_STATE 32
+#define BLYSS_IDX_ROLL  36
+#define BLYSS_IDX_TOKEN 44
+
+/* timing setup ============================================================= */
+#define HEADER_TIME 2400  // us
+#define T1          400   // us
+#define T2          800   // us
+#define FOOTER_TIME 24    // ms
+
+/* private constants ======================================================== */
+static const char error[] = "Error";
+static const char ok[] = "Ok";
+static const uint8_t barrel[] = { 0x98, 0xDA, 0x1E, 0xE6, 0x67 };
+static const uint8_t channels[] = { 0, 8, 4, 2, 1, 3, 5, 6, 7 };
+
 /* structures =============================================================== */
-/* types ==================================================================== */
-/* private variables ======================================================== */
+struct xBlyss {
+  xDoutPort * out;
+  xDinPort  * in;
+  uint8_t last_token;
+  uint8_t roll_idx;
+};
+
 /* private functions ======================================================== */
-/* public variables ========================================================= */
+// -----------------------------------------------------------------------------
+static inline int
+iSetOut (xBlyss * b) {
+
+  return iDoutSet (0, b->out);
+}
+
+// -----------------------------------------------------------------------------
+static inline int
+iClearOut (xBlyss * b) {
+
+  return iDoutClear (0, b->out);
+}
+
+// -----------------------------------------------------------------------------
+static int
+iSendBit (xBlyss * b, bool bit) {
+
+  if (iClearOut (b) == 0) {
+
+    delay_us (bit ? T1 : T2);
+
+    if (iSetOut (b) == 0) {
+
+      delay_us (bit ? T2 : T1);
+      return 0;
+    }
+  }
+  return -1;
+}
+
+// -----------------------------------------------------------------------------
+static inline int
+iSendHeader (xBlyss * b) {
+
+  if (iSetOut (b) == 0) {
+
+    delay_us (HEADER_TIME);
+    return 0;
+  }
+  return -1;
+}
+
+// -----------------------------------------------------------------------------
+static inline int
+iSendFooter (xBlyss * b) {
+
+  if (iClearOut (b) == 0) {
+
+    delay_ms (FOOTER_TIME);
+    return 0;
+  }
+  return -1;
+}
+
+// -----------------------------------------------------------------------------
+static inline int
+iSendFrame (xBlyss * b, xBlyssFrame * f) {
+  uint8_t byte;
+  uint8_t mask = 0;
+
+  if (iSendHeader (b)) {
+
+    return -1;
+  }
+
+  for (uint8_t i = 0; i < BLYSS_NOF_BITS; i++) {
+
+    if (mask == 0) {
+
+      mask = 0x80;
+      byte = f->raw[i / 8];
+    }
+
+    if (iSendBit (b, byte & mask)) {
+
+      return -1;
+    }
+    mask >>= 1;
+  }
+
+  return iSendFooter (b);
+}
+
+
+// -----------------------------------------------------------------------------
+static void
+vSetBits (xBlyssFrame * f, uint8_t index, uint8_t len, uint16_t value) {
+  uint8_t i = index / 8;
+  uint8_t mask, ls;
+
+  for (uint8_t c = 0; c < len; c += 4, index += 4, i++) {
+
+    if ( (index % 8) == 0) {
+
+      // index multiple de 8 (MSB)
+      if ( (len - c) >= 8) {
+
+        // copie un octet
+        f->raw[i] = value >> c;
+        c += 4;
+        index += 4;
+        continue;
+      }
+
+      // quartet MSB
+      mask = 0x0F;
+      ls = 4;
+    }
+    else {
+
+      // quartet LSB
+      mask = 0xF0;
+      ls = 0;
+    }
+
+    // copie quartet
+    f->raw[i] = (f->raw[i] & mask) | ( (value >> ( (len - c) - 4)) << ls);
+  }
+}
+
+// -----------------------------------------------------------------------------
+static uint16_t
+usGetBits (const xBlyssFrame * f, uint8_t index, uint8_t len) {
+  uint16_t value = 0;
+  uint8_t i = index / 8;
+  uint8_t nibble, rest;
+
+  for (uint8_t c = 0; c < len; c += 4, index += 4, i++) {
+
+    rest = (len - c);
+
+    if ( (index != 4) && ( (index % 8) == 0)) {
+
+
+      // index multiple de 8 (MSB)
+      if (rest >= 8) {
+
+        // copie un octet
+        value |=  f->raw[i] << (rest - 8);
+        c += 4;
+        index += 4;
+        continue;
+      }
+
+      // quartet MSB
+      nibble = f->raw[i] >> 4;
+    }
+    else {
+
+      // quartet LSB
+      nibble = f->raw[i] & 0x0F;
+    }
+
+    // copie quartet
+    value |= nibble << (rest - 4);
+  }
+
+  return value;
+}
+
+
+// -----------------------------------------------------------------------------
+static inline void
+vSetFlag (xBlyssFrame * f) {
+
+  vSetBits (f, BLYSS_IDX_FLAG, 8, BLYSS_FLAG);
+}
+
+// -----------------------------------------------------------------------------
+static inline uint8_t
+ucFlag (const xBlyssFrame * f) {
+
+  return usGetBits (f, BLYSS_IDX_FLAG, 8);
+}
+
+// -----------------------------------------------------------------------------
+static inline uint8_t
+ucToken (const xBlyssFrame * f) {
+
+  return usGetBits (f, BLYSS_IDX_TOKEN, 8);
+}
+
+// -----------------------------------------------------------------------------
+static inline void
+vSetToken (xBlyss * b, xBlyssFrame * f) {
+
+  vSetBits (f, BLYSS_IDX_TOKEN, 8, b->last_token);
+  b->last_token += 10;
+}
+
+// -----------------------------------------------------------------------------
+static inline uint8_t
+ucRollingCode (const xBlyssFrame * f) {
+
+  return usGetBits (f, BLYSS_IDX_ROLL, 8);
+}
+
+// -----------------------------------------------------------------------------
+static inline void
+vSetRollingCode (xBlyss * b, xBlyssFrame * f) {
+
+  vSetBits (f, BLYSS_IDX_ROLL, 8, barrel[b->roll_idx]);
+  if (++b->roll_idx >= sizeof (barrel)) {
+
+    b->roll_idx = 0;
+  }
+}
+
+// -----------------------------------------------------------------------------
+static bool
+bIsValidRollingCode (uint8_t code) {
+
+  for (uint8_t i = 0; i < sizeof (barrel); i++) {
+
+    if (barrel[i] == code) {
+
+      return true;
+    }
+  }
+  return false;
+}
+
+// -----------------------------------------------------------------------------
+static uint8_t
+ucDecodeChannel (uint8_t field) {
+  uint8_t c;
+
+  for (c = 0; c < sizeof (channels); c++) {
+
+    if (field == channels[c]) {
+
+      return c;
+    }
+  }
+  return -1;
+}
+
+
 /* internal public functions ================================================ */
 
 // -----------------------------------------------------------------------------
+xBlyss *
+xBlyssNew (int iOutPin, int iInPin) {
+  xBlyss * b = calloc (1, sizeof (xBlyss));
+  assert (b);
+
+  if (iOutPin >= 0) {
+    xDout pin = { .num = iOutPin, .act = true };
+
+    b->out = xDoutOpen (&pin, 1);
+    if (b->out == NULL) {
+
+      goto xBlyssNewError;
+    }
+  }
+
+  if (iInPin >= 0) {
+    xDin pin = { .num = iInPin, .act = true, .pull = false };
+
+    b->in = xDinOpen (&pin, 1);
+    if (b->in == NULL) {
+
+      goto xBlyssNewError;
+    }
+  }
+
+  b->last_token = 0x7D;
+
+  return b;
+
+xBlyssNewError:
+  free (b);
+  return NULL;
+}
+
+// -----------------------------------------------------------------------------
+int
+iBlyssDelete (xBlyss * b) {
+  int ret = 0;
+
+  if (b->out) {
+
+    ret = iDoutClose (b->out);
+  }
+  if (b->in) {
+
+    ret = iDinClose (b->in);
+  }
+  free (b);
+  return ret;
+}
+
+// -----------------------------------------------------------------------------
+int
+iBlyssSend (xBlyss * b, xBlyssFrame * f, uint8_t repeat) {
+
+  if (b->out) {
+    vSetRollingCode (b, f);
+    vSetToken (b, f);
+
+    while (repeat--) {
+
+      if (iSendFrame (b, f)) {
+
+        return -1;
+      }
+    }
+  }
+
+  return 0;
+}
+
+
+// -----------------------------------------------------------------------------
+xBlyssFrame *
+xBlyssNewFrame (const uint8_t * id) {
+
+  xBlyssFrame * f = calloc (1, sizeof (xBlyssFrame));
+  assert (f);
+
+  vSetFlag (f);
+  vBlyssSetTxId (f, id);
+  return f;
+}
+
+// -----------------------------------------------------------------------------
+void
+vBlyssSetTxId (xBlyssFrame * f, const uint8_t * id) {
+
+  vBlyssSetGlobalChannel (f, id[0]);
+  vBlyssSetAddress (f, (id[1] << 8) |  id[2]);
+}
+
+// -----------------------------------------------------------------------------
+void
+vBlyssSetGlobalChannel (xBlyssFrame * f, uint8_t global_channel) {
+
+  vSetBits (f, BLYSS_IDX_GCHAN, 4, global_channel);
+}
+
+// -----------------------------------------------------------------------------
+void
+vBlyssSetAddress (xBlyssFrame * f, uint16_t addr) {
+
+  vSetBits (f, BLYSS_IDX_ADDR, 16, addr);
+}
+
+// -----------------------------------------------------------------------------
+void
+vBlyssSetChannel (xBlyssFrame * f, uint8_t channel) {
+
+  if (channel < (sizeof (channels) - 1)) {
+
+    vSetBits (f, BLYSS_IDX_CHAN, 4, channels[channel]);
+  }
+}
+
+// -----------------------------------------------------------------------------
+void
+vBlyssSetState (xBlyssFrame * f, bool state) {
+
+  vSetBits (f, BLYSS_IDX_STATE, 4, ! state);
+}
+
+// -----------------------------------------------------------------------------
+uint8_t
+ucBlyssGlobalChannel (const xBlyssFrame * f) {
+
+  return usGetBits (f, BLYSS_IDX_GCHAN, 4);
+}
+
+// -----------------------------------------------------------------------------
+uint8_t
+ucBlyssChannel (const xBlyssFrame * f) {
+
+  return ucDecodeChannel (usGetBits (f, BLYSS_IDX_CHAN, 4));
+}
+
+// -----------------------------------------------------------------------------
+uint16_t
+usBlyssAddress (const xBlyssFrame * f) {
+
+  return usGetBits (f, BLYSS_IDX_ADDR, 16);
+}
+
+// -----------------------------------------------------------------------------
+bool
+bBlyssState (const xBlyssFrame * f) {
+
+  return ! usGetBits (f, BLYSS_IDX_STATE, 4);
+}
+
+// -----------------------------------------------------------------------------
+bool
+bBlyssIsValidFrame (const void * buffer) {
+  const xBlyssFrame * f = buffer;
+
+  if (f->raw[0] == BLYSS_FLAG) {
+
+    return bIsValidRollingCode (ucRollingCode (f));
+  }
+  return false;
+}
+
+// -----------------------------------------------------------------------------
+bool 
+bBlyssIsValidChannel (uint8_t channel) {
+  
+  return ucDecodeChannel (channel) != (uint8_t) -1;
+}
+
+// -----------------------------------------------------------------------------
+void
+vBlyssPrintFrameToFile (const xBlyssFrame * f, FILE * out) {
+  uint16_t i;
+
+  printf ("RF frame          : ");
+  for (i = 0; i < BLYSS_NOF_BITS; i += 4) {
+
+    printf ("%X", usGetBits (f, i, 4));
+  }
+
+  i = ucFlag (f);
+  printf ("\nRF footprint      : %02X - %s\n", i, i == BLYSS_FLAG ? ok : error);
+  printf ("RF global channel : %X\n", ucBlyssGlobalChannel (f));
+  printf ("RF adress         : %04X\n", usBlyssAddress (f));
+
+  printf ("RF channel        : %u\n", ucBlyssChannel (f));
+  printf ("Light status      : %s\n", bBlyssState (f) ? "On" : "Off");
+
+  i = ucRollingCode (f);
+  printf ("Rolling code      : %02X - %s\n", i, bIsValidRollingCode (i) ? ok : error);
+  printf ("Token             : %02X\n", ucToken (f));
+}
 
 /* ========================================================================== */
