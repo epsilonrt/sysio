07e271104cc4c076a0bb9f25725e08eb151339ca
epsilonrt <epsilonrt@gmail.com>
2018-03-06 07:52:45 +0100

    Adds template workspaces

--- dev/template/cpp/template.cpp
+++ dev/template/cpp/template.cpp
@@ -1,43 +1,38 @@
-/*
- * template.cpp
- * @brief Description of your program
- */
+// template
+
+// You should replace its source code with yours and explain what it does here:
+// This program flashes an LED connected to a GPIO pin with a frequency of 2 Hz.
+
 #include <iostream>
-#include <cstdlib>
-#include <cassert>
-#include <csignal>
-#include <sysio/delay.h>
+#include <sysio/clock.h>
+#include <sysio/gpio.h>
 
 using namespace std;
+using namespace Sysio;
 
-/* constants ================================================================ */
-/* macros =================================================================== */
-/* structures =============================================================== */
-/* types ==================================================================== */
-/* private variables ======================================================== */
-/* public variables ========================================================= */
+Clock clk; // the clock of our program used for time calculation... 
+Gpio g; // our GPIO port
 
-/* private functions ======================================================== */
+// !!! Be careful !!!
+// Check before launching this program that the pin below is well connected to an LED!
+const int ledPin = 0; // Header Pin 11: GPIO17 for RPi, GPIOA0 for NanoPi
+
 // -----------------------------------------------------------------------------
-static void
-vSigIntHandler (int sig) {
+int main (int argc, char **argv) {
 
-  cout << endl << "everything was closed." << endl << "Have a nice day !" << endl;
-  exit (EXIT_SUCCESS);
-}
-
-/* main ===================================================================== */
-int
-main (int argc, char **argv) {
+  Pin & led = g.pin(ledPin); // led is a reference on pin 11 of the GPIO
+  
+  g.open(); // Do not forget to open the GPIO before changing the pins !
+  led.setMode(Pin::ModeOutput); // the led pin is an output
+  led.write(0); // turn off the led
 
   // Replace the lines below with your code.
-  // vSigIntHandler() intercepte le CTRL+C
-  signal (SIGINT, vSigIntHandler);
-
   cout << "Press Ctrl+C to abort ..." << endl;
   for (;;) {
-    cout << '.' << flush;
-    delay_ms (1000);
+
+    led.toggle(); // blinking led
+    clk.delay (1000);
+    cout << '.' << flush; // print one point per second
   }
 
   return 0;
