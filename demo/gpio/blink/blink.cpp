91be6a89bc6dbcefb087168ded64a00e814b75ba
epsilonrt <epsilonrt@gmail.com>
2018-03-07 22:37:44 +0100

    Adds example workspaces

--- demo/gpio/blink/blink.cpp
+++ demo/gpio/blink/blink.cpp
@@ -7,29 +7,43 @@
 
 // This example code is in the public domain.
 #include <iostream>
+#include <csignal>
+#include <cstdlib>
 #include <sysio/clock.h>
 #include <sysio/gpio.h>
 
 using namespace std;
 using namespace Sysio;
 
-Clock clk; // the clock of our program used for time calculation... 
-Gpio g; // our GPIO port
-
-// !!! Be careful !!!
-// Check before launching this program that the pin below is well connected to an LED!
+// <DANGER> Be careful !!! Before launching this program :
+//    -> Check that the pin below is well connected to an LED ! <-
 const int ledPin = 0; // Header Pin 11: GPIO17 for RPi, GPIOA0 for NanoPi
+
+Clock clk; // the clock of our program used for time calculation...
+Gpio g; // our GPIO port
+Pin & led = g.pin (ledPin); // led is a reference on pin 11 of the GPIO
+
+// -----------------------------------------------------------------------------
+// Signal Handler
+static void
+sighandler (int sig) {
+
+  g.close();
+  printf ("\neverything was closed.\nHave a nice day !\n");
+  exit (EXIT_SUCCESS);
+}
 
 // -----------------------------------------------------------------------------
 int main (int argc, char **argv) {
 
-  Pin & led = g.pin(ledPin); // led is a reference on pin 11 of the GPIO
-  
-  g.open(); // Do not forget to open the GPIO before changing the pins !
-  led.setMode(Pin::ModeOutput); // the led pin is an output
-  led.write(0); // turn off the led
 
-  // Replace the lines below with your code.
+  g.open();
+  led.setMode (Pin::ModeOutput); // the led pin is an output
+  led.write (0); // turn off the led
+
+  // sighandler() intercepts CTRL+C
+  signal (SIGINT, sighandler);
+  signal (SIGTERM, sighandler);
   cout << "Press Ctrl+C to abort ..." << endl;
   for (;;) {
 
